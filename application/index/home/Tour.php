89cc9191629dcc35b30e1e6ff682a75669437893
Lucas <18674012767@163.com>
2019-02-15 15:35:12 +0800

    前台样式调整

--- application/index/home/Tour.php
+++ application/index/home/Tour.php
@@ -20,4 +20,34 @@ class Tour extends Base
     	$this->assign('toursArr',$toursArr);
         return $this->fetch();
     }
+
+        public function detail()
+    {
+        $id = input('get.article_id');
+        // 浏览量 +1
+        ArticleModel::where('article_id',$id)->setInc('click');
+        // 获取当前文章详情
+        $row = ArticleModel::find($id);
+        // 获取当前文章的评论
+        $comments = $row->comment()->with('member')->where(['is_show'=>1,'status'=>1])->order('ctime desc')->limit(4)->select();
+        //halt($comments);
+        // 获取推荐的文章
+        $tuiWhere = [
+            ['cate_id' ,'eq' , $row['cate_id']],
+            ['is_stick', 'eq', 1],
+            ['is_show' ,'eq', 1],
+            ['status' ,'eq', 1],
+            ['article_id' ,'neq',$id]
+        ];
+        $tuiArticles = ArticleModel::where($tuiWhere)->field('thumb,article_id,article_title')->order('click desc')->limit(4)->select();
+        //halt(ArticleModel::getLastSql());
+        
+        //halt($tuiArticles);
+        $this->assign('cateID',$row['cate_id']);
+        $this->assign('tuiArticles',$tuiArticles);
+        
+        $this->assign('comments',$comments);
+        $this->assign('data_info',$row);
+        return $this->fetch();
+    }
 }
