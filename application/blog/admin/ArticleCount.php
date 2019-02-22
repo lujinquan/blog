edcdbbcb4a829d5125161c839d2fbfba2c500e3d
Lucas <18674012767@163.com>
2019-02-22 17:31:44 +0800

    前台页面导航栏上部多2px空白问题解决

--- application/blog/admin/ArticleCount.php
+++ application/blog/admin/ArticleCount.php
@@ -9,13 +9,25 @@ class ArticleCount extends Admin
 {
     public function index()
     {
-    	$where = [
-    		['status','eq',1],
-    		['is_show','eq',1]
-    	];
-    	$articles = ArticleModel::with('cate')->where($where)->field('count(article_id) as article_ids,cate_id')->group('cate_id')->select();
-    	//halt($articles);
-    	$this->assign('articles',$articles);
+        if ($this->request->isAjax()) {
+            $where = [
+                ['status','eq',1],
+                ['is_show','eq',1]
+            ];
+            $articles = ArticleModel::with('cate')->where($where)->field('count(article_id) as article_ids,cate_id')->group('cate_id')->select();
+            $data['code'] = 0;
+            $data['msg'] = '';
+            return json($data);
+        }
+        $where = [
+            ['status','eq',1],
+            ['is_show','eq',1]
+        ];
+    	$data['data'] = ArticleModel::with('cate')->where($where)->field('count(article_id) as article_ids,cate_id')->group('cate_id')->select()->toArray();
+        $data['code'] = 0;
+        $data['msg'] = '';
+    	//halt(json($data));
+    	$this->assign('articles',json_encode($data));
     	return $this->fetch();
     }
 }
