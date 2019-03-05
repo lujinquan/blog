a115bfdc8b727976a76ec64a7e738be6a2803540
demo <992924033@qq.com>
2019-03-05 23:06:49 +0800

    添加接口模块

---
+++ application/route.php
@@ -0,0 +1,5 @@
+<?php
+use think\Route;
+
+//Route::domain('web','v1');
+Route::rule('user/:id','v1/user/login');
---
+++ application/v1/home/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\v1\home;
+
+class User
+{
+	public function index(){
+		echo '模块是v1,控制器是User,方法是index';
+	}
+
+	public function login(){
+		echo '模块是v1,控制器是User,方法是login';
+	}
+}
