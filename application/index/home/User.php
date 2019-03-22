f7a42f159e2b3aa7b22a16205f240914f6f8237b
Lucas <18674012767@163.com>
2019-03-22 17:37:25 +0800

    登录功能

--- application/index/home/User.php
+++ application/index/home/User.php
@@ -2,12 +2,44 @@
 
 namespace app\index\home;
 
+use think\Validate;
 use app\index\home\Base;
+use app\blog\model\Member as MemberModel;
 
 class User extends Base
 {
+	public function login()
+	{
+		return $this->fetch();
+	}
+
 	public function register()
 	{
+		if ($this->request->isPost()) {
+            $data = $this->request->post();
+            if(!$data['is_agree']){
+            	return $this->error('未同意相关守则和法律政策');
+            }
+            $data['salt'] = substr(uniqid(),0,6);
+            $data['password'] = md5($data['password'].$data['salt']);
+            $data['password_confirm'] = md5($data['password_confirm'].$data['salt']);
+
+
+            $validate = new Validate([
+			    'nick|昵称'       => 'require|unique:system_user',
+			    'email|邮箱' => 'require|email',
+			    'password|密码'   => 'require|length:32|confirm',
+			]);
+            if (!$validate->check($data)) {
+            	halt($validate->getError());
+			    return $this->error($validate->getError());
+			}
+            $mod = new MemberModel();
+            if (!$mod->allowField(true)->create($data)) {
+                return $this->error('添加失败');
+            }
+            return $this->success('添加成功');
+        }
 		return $this->fetch();
 	}
 }
